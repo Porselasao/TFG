61eb51e5690c2b1f71c7e785d9cab8eeab10cc02
Francisco Porcel <francisco-pr@hotmail.com>
2016-06-19 21:24:18 +0200

    Hecho el pintar_campo para las estaciones. Ploteados los casos nuevos de las estaciones. Completados los casos de dirección de radiación genérica. Comenzado el análisis de las estaciones base.

--- 2016-05-29/2_estaciones/pintar_campo.m
+++ 2016-05-29/2_estaciones/pintar_campo.m
@@ -3,13 +3,13 @@
 cargar=1;
 
 if cargar
-clc
+% clc
 clear all
-close all
+% close all
 
 cd resultados
-load caso_3_ejecucion_1clusters1.mat
-caso='Caso base';
+load caso_65_ejecucion_1clusters1.mat
+caso='Caso 5. ';
 cd ..
 cargar=1;
 end
@@ -17,122 +17,95 @@ end
 % Se ha de seleccionar para el campo Etot{} el siguiente orden:
 % {contador_antenas,contador_sensores,contador_phi,contador_theta}
 
-for i=1:tiempo_de_simulacion
-    plot3(posicion_X{1}(:,i),posicion_Y{1}(:,i),posicion_Z{1}(:,i),'.')
-    axis([0 30 0 30 0 30])
-    grid on;
-end
+% for i=1:tiempo_de_simulacion
+%     plot3(posicion_X{1}(:,i),posicion_Y{1}(:,i),posicion_Z{1}(:,i),'.')
+%     axis([0 30 0 30 0 30])
+%     grid on;
+% end
 
 c_antenas=2;
 c_sensor=1;
-c_phi=2;
-c_theta=1;
 instante=1;
 
 representacion_polar=1;
-representacion_2D=1;
+representacion_2D=0;
 representacion_3D=0;
 normalizado=1;
-funcion_theta=1;
+funcion_theta=0;
 
-vector_thetas=theta;
-vector_phis=phi;
 
-for i=1:tiempo_de_simulacion
-    eficiencia(i)=minimo_beam{c_antenas,c_sensor,c_phi,c_theta,i}./minimo_beam1{c_antenas,c_sensor,c_phi,c_theta,i};
-end
+[distancia_deseada,vector_thetas,vector_phis]=cart2sph2(estacion(1,:)-mejor_centro_cluster(1,:));
+% vector_thetas=theta_clister_deseada;
+% vector_phis=phi;
 
-for i=1:tiempo_de_simulacion
-    clustercillo(i)=max(idx_global{c_antenas,c_sensor,c_phi,c_theta,i});
-end
 
-maximo_cluster=max(clustercillo)
+% for i=1:tiempo_de_simulacion
+%     eficiencia(i)=minimo_beam{c_antenas,c_sensor,c_phi,c_theta,i}./minimo_beam1{c_antenas,c_sensor,c_phi,c_theta,i};
+% end
+% 
+% for i=1:tiempo_de_simulacion
+%     clustercillo(i)=max(idx_global{c_antenas,c_sensor,c_phi,c_theta,i});
+% end
 
-phi_apuntado=vector_phis(c_phi)
-theta_apuntado=vector_thetas(c_theta)
+% % maximo_cluster=max(clustercillo)
+% % 
+% % phi_apuntado=vector_phis(c_phi)
+% % theta_apuntado=vector_thetas(c_theta)
 
-eficiencia
-eficiencia_minima=min(eficiencia)
-eficiencia_media=mean(eficiencia)
+% eficiencia
+% % eficiencia_minima=min(eficiencia)
+% % eficiencia_media=mean(eficiencia)
 
 if cargar
-k=1;
-[Etot{c_antenas,c_sensor,c_phi,c_theta,instante,k},Ptot{c_antenas,c_sensor,c_phi,c_theta,instante,k},directividad{c_antenas,c_sensor,c_phi,c_theta,instante,k},Thmax{c_antenas,c_sensor,c_phi,c_theta,instante,k},Phmax{c_antenas,c_sensor,c_phi,c_theta,instante,k},directividad_buscada{c_antenas,c_sensor,c_phi,c_theta,instante,k}] = ...
-    Calacula_Campo_entero(array_resultado{c_antenas,c_sensor,c_phi,c_theta,1}(:,:,mejor_idx==k),freq_config,range_config,deltheta,delphi,vector_thetas(c_theta),vector_phis(c_phi));
-            
+[Etot{c_antenas,c_sensor,1,1,1,1},yei,you,yai,yiu,kkkk] = ...
+  Calacula_Campo_entero(array_resultado{c_antenas,c_sensor,1}(:,:,mejor_idx==k),freq_config,range_config,deltheta,delphi,vector_thetas,vector_phis);
+           
 i=1;
-[Etot1{c_antenas,c_sensor,c_phi,c_theta,instante,i},Ptot1{c_antenas,c_sensor,c_phi,c_theta,instante,i},directividad1{c_antenas,c_sensor,c_phi,c_theta,instante,i},Thmax1{c_antenas,c_sensor,c_phi,c_theta,instante,i},Phmax1{c_antenas,c_sensor,c_phi,c_theta,instante,i},directividad_buscada1{c_antenas,c_sensor,c_phi,c_theta,instante,i}] = Calacula_Campo_entero(array_resultado1{c_antenas,c_sensor,c_phi,c_theta,instante}(:,:,i),freq_config,range_config,deltheta1,delphi1,thetabuscado,phibuscado);
+[Etot1{c_antenas,c_sensor,1,1,1,1},aaa,b,c,d,e] = ...
+    Calacula_Campo_entero(array_resultado{c_antenas,c_sensor,1}(:,:,1),freq_config,range_config,deltheta1,delphi1,0,0);
+                                 
+
 end
 %% Diagrama de radiación en 2D (polar)
 
 if funcion_theta
-    
-vector_theta = 0:deltheta:180;
-vector_phi = 0:delphi:360;
-t=vector_phi/360*2*pi;
-
-
-if representacion_polar
-
-figure;
-titulo='Diagrama de radiación (polar). \it\theta = 90º, \phi (º). ';
-for i=1:(tiempo_de_simulacion)
-    LineSpec.LineWidth=2;
-    P=polar(t,1.00001*ones(size(t)));
-    set(P, 'Visible', 'off')
-    hold on
-    if normalizado
-        polar(t(1:end),[Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:) Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,1)]/max(Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:)),'r');
-        polar(t(1:end),[Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:) Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,1)]/max(Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:)),LineSpec);
-    else
-        polar(t(1:end),[Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:) Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,1)],'r');
-        polar(t(1:end),[Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:) Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,1)],LineSpec);
-    end
-    hold off
-    title([titulo caso])
-end
-
-
-
-end
-
-if representacion_2D
-t=t(1:end)*360/(2*pi);
-figure
-for i=1:(tiempo_de_simulacion)
-    if normalizado
-        plot(t,[Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:) Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,1)]/max(Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:)))
-    else
-        plot(t,[Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,:) Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(vector_thetas(c_theta)+1,1)])
-    end
-        grid on;
-end
-end
 
 else
    
+if c_antenas==1
+    tipo_de_antena='isotrópicas.';
+else
+    tipo_de_antena='dipolo.';
+end
+
 vector_theta = 0:deltheta:180;
 vector_phi = 0:delphi:360;
-t=vector_phi/360*2*pi;
+t=vector_theta/360*2*pi;
 
+
+x=[vector_thetas-1 vector_thetas-1]/360*2*pi;
+y=[0 1.5];
+
+vector_thetas=round(vector_thetas);
+vector_phis=round(vector_phis);
 
 if representacion_polar
 figure;
-titulo='Diagrama de radiación (polar). \it\theta = 90º, \phi (º). ';
+% titulo=[caso num2str(sensores) ' sensores, antenas ' tipo_de_antena ' Diagrama de radiación (polar). \it\theta = ' num2str(vector_thetas(c_theta)) ' º, \phi = (º)'];%...
+titulo=[caso num2str(sensores) ' sensores, antenas ' tipo_de_antena ' \it\phi = ' num2str(vector_phis(1)) 'º, \theta = (º)'];
 for i=1:(tiempo_de_simulacion)
     LineSpec.LineWidth=2;
     P=polar(t,1.00001*ones(size(t)));
     set(P, 'Visible', 'off')
     hold on
-    if normalizado
-        polar(t(1:end),[Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)' Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)' Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(1,vector_phis(c_phi)+1)]/max(Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)'),'r');
-        polar(t(1:end),[Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)' Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)' Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(1,vector_phis(c_phi)+1)]/max(Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)'),LineSpec);
-    else
-        polar(t(1:end),[Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)' Etot1{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)'],'r');
-        polar(t(1:end),[Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)' Etot{c_antenas,c_sensor,c_phi,c_theta,i,1}(:,vector_phis(c_phi)+1)'],LineSpec);
-    end
+    
+    polar(t(1:end),[Etot1{c_antenas,c_sensor,1,1,1,1}(:,vector_phis(1)+1)' Etot1{c_antenas,c_sensor,1,1,1,1}(1,vector_phis(1)+1)]/max(Etot1{c_antenas,c_sensor,1,1,1,1}(:,vector_phis(1)+1)'),'r');
+    polar(t(1:end),[Etot{c_antenas,c_sensor,1,1,1,1}(:,vector_phis(1)+1)' Etot{c_antenas,c_sensor,1,1,1,1}(1,vector_phis(1)+1)]/max(Etot1{c_antenas,c_sensor,1,1,1,1}(:,vector_phis(1)+1)'),LineSpec);
+    
+    polar(x,y,'k')
     hold off
-    title([titulo caso])
+    title([titulo])
+    xlabel(['Dirección de radiación \it\phi = ' num2str(vector_phis(1)) 'º, \theta = ' num2str(vector_thetas(1))])
 end
 end
 
